af3b3880062d272f6ceb6da270beb4ce542c8284
Michael Andrews <72107310+mandrewspicard@users.noreply.github.com>
2023-06-15 22:09:20 -0700

    Sproingy Arm

--- Assets/Scripts/ArmController.cs
+++ Assets/Scripts/ArmController.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class ArmController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Onboard components
+    private BoxCollider2D mainCollider;
+    private Rigidbody2D rbArm;
+    private Vector3 armPos;
+
+    // Offboard components
+    public GameObject target;
+    private Transform targetPos;
+
+
+    // Tuning variables
+    public float growSpeed = 0.05f;
+
+
+
     void Start()
     {
+        // Find components
+        targetPos = target.GetComponent<Transform>();
+        rbArm = GetComponent<Rigidbody2D>();
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (targetPos.position.x > 0f)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 180.0f / Mathf.PI * Mathf.Atan((targetPos.position.y - transform.position.y) / (targetPos.position.x - transform.position.x)));
+        }
+
+        else
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 180f + 180.0f / Mathf.PI * Mathf.Atan((targetPos.position.y - transform.position.y) / (targetPos.position.x - transform.position.x)));
+        }
+
+
+        if (Vector3.Distance(transform.position, targetPos.position) - transform.localScale.x > 0.05 && Vector3.Distance(transform.position, targetPos.position) > transform.localScale.x)
+        {
+            transform.localScale = new Vector3(transform.localScale.x + growSpeed, 1f, 1f);
+        }
+        else if (Vector3.Distance(transform.position, targetPos.position) - transform.localScale.x < -0.05)
+        {
+            transform.localScale = new Vector3(transform.localScale.x - growSpeed, 1f, 1f);
+        }
+
     }
+
 }
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+
+    private CircleCollider2D mainCollider;
+    private Rigidbody2D rbEnemy;
+
+    private float moveSpeed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCollider = GetComponent<CircleCollider2D>();
+        rbEnemy = GetComponent<Rigidbody2D>();
+        rbEnemy.velocity = new Vector2(moveSpeed, 0);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < -2)
+        {
+            rbEnemy.velocity = new Vector2(moveSpeed, 0);
+        }
+        else if (transform.position.x > 2)
+        {
+            rbEnemy.velocity = new Vector2(-moveSpeed, 0);
+        }
     }
+
+    void OnTriggerEnter2D(Collider2D touched)
+    {
+        transform.position = new Vector3(0, 1, 0);
+    }
+
 }
