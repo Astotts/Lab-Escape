f9820a92efac957d6557a381610c4cb4759f0eaa
Astotts <55721055+Astotts@users.noreply.github.com>
2023-06-27 01:30:23 -0700

    Fixed Tentacles Scripts

--- Assets/C# Scripts/Player/Weapons/BuzzSaw.cs
+++ Assets/C# Scripts/Player/Weapons/BuzzSaw.cs
@@ -53,6 +53,7 @@ public class BuzzSaw : GenericWeapon
                 enemyList.Remove(enemy.gameObject);
             }
             else{
+                Debug.Log(enemy.GetComponent<DamageController>());
                 enemy.GetComponent<DamageController>().TakeDamage(damage);
                 Debug.Log("BuzzSaw has damaged the Enemy");
             } 
